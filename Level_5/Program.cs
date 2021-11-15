fc9ca4eab396f94108add74b28c8e8ef6a90c8a0
SpYDeR <levashov_vanya@mail.ru>
2021-11-15 23:31:47 +0300

    Add H/W

---
+++ Level_5/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Level_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            tree.AddItem(10);
+            tree.AddItem(20);
+            tree.AddItem(15);
+            tree.AddItem(59);
+            tree.AddItem(12);
+            tree.AddItem(28);
+            tree.AddItem(28);
+            tree.AddItem(35);
+            tree.AddItem(8);
+            tree.AddItem(5);
+            tree.PrintTree();
+            Console.WriteLine("======================================");
+            TreeNode BFSout1 = BFS(tree, 8);
+            TreeNode DFSout1 = DFS(tree, 5);
+            TreeNode BFSout2 = BFS(tree, 5);
+            TreeNode DFSout2 = DFS(tree, 8);
+            TreeNode BFSout3 = BFS(tree, 125);
+            TreeNode DFSout3 = DFS(tree, 512);
+            TreeNode BFSout4 = BFS(tree, 512);
+            TreeNode DFSout4 = DFS(tree, 125);
+        }
+        static TreeNode BFS(Tree tree, int value)
+        {
+            Console.WriteLine("=====Начало BFS Поиска=====");
+            var root = tree.GetRoot();
+            Queue<TreeNode> roots = new Queue<TreeNode>();
+            if (root.Value == value)
+            {
+                Console.WriteLine("Значение найдено");
+                Console.WriteLine("=====Конец BFS Поиска=====");
+                return root;
+            }
+            else
+            {
+                if (root.RightChild != null)
+                {
+                    roots.Enqueue(root.RightChild);
+                    Console.WriteLine($"Правая ветка со значением {root.RightChild.Value} добавлена в очередь");
+                }
+                if (root.LeftChild != null)
+                {
+                    roots.Enqueue(root.LeftChild);
+                    Console.WriteLine($"Левая ветка со значением {root.LeftChild.Value} добавлена в очередь");
+                }
+                if (roots.Count == 0)
+                {
+                    Console.WriteLine("Очердь пуста, значение не найдено");
+                    Console.WriteLine("=====Конец BFS Поиска=====");
+                    return null;
+                }
+                else
+                {
+                    Console.WriteLine($"Ветка со значением {root.Value} убирается из очереди");
+                    root = roots.Dequeue();
+                    while (true)
+                    {
+                        if (root.Value == value)
+                        {
+                            Console.WriteLine("Значение найдено");
+                            Console.WriteLine("=====Конец BFS Поиска=====");
+                            return root;
+                        }
+                        else
+                        {
+                            if (root.RightChild != null)
+                            {
+                                roots.Enqueue(root.RightChild);
+                                Console.WriteLine($"Правая ветка со значением {root.RightChild.Value} добавлена в очередь");
+                            }
+                            if (root.LeftChild != null)
+                            {
+                                roots.Enqueue(root.LeftChild);
+                                Console.WriteLine($"Левая ветка со значением {root.LeftChild.Value} добавлена в очередь");
+                            }
+                            if (roots.Count == 0)
+                            {
+                                Console.WriteLine("Очердь пуста, значение не найдено");
+                                Console.WriteLine("=====Конец BFS Поиска=====");
+                                return null;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Ветка со значением {root.Value} убирается из очереди");
+                                root = roots.Dequeue();
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+        static TreeNode DFS(Tree tree, int value)
+        {
+            Console.WriteLine("=====Начало DFS Поиска=====");
+            var root = tree.GetRoot();
+            Stack<TreeNode> roots = new Stack<TreeNode>();
+            if (root.Value == value)
+            {
+                Console.WriteLine("Значение найдено");
+                Console.WriteLine("=====Конец DFS Поиска=====");
+                return root;
+            }
+            else
+            {
+                if (root.RightChild != null)
+                {
+                    roots.Push(root.RightChild);
+                    Console.WriteLine($"Правая ветка со значением {root.RightChild.Value} добавлена в стак");
+                }
+                if (root.LeftChild != null)
+                {
+                    roots.Push(root.LeftChild);
+                    Console.WriteLine($"Левая ветка со значением {root.LeftChild.Value} добавлена в стак");
+                }
+                if (roots.Count == 0)
+                {
+                    Console.WriteLine("Очердь пуста, значение не найдено");
+                    Console.WriteLine("=====Конец DFS Поиска=====");
+                    return null;
+                }
+                else
+                {
+                    Console.WriteLine($"Ветка со значением {root.Value} убирается из стака");
+                    root = roots.Pop();
+                    while (true)
+                    {
+                        if (root.Value == value)
+                        {
+                            Console.WriteLine("Значение найдено");
+                            Console.WriteLine("=====Конец DFS Поиска=====");
+                            return root;
+                        }
+                        else
+                        {
+                            if (root.RightChild != null)
+                            {
+                                roots.Push(root.RightChild);
+                                Console.WriteLine($"Правая ветка со значением {root.RightChild.Value} добавлена в стак");
+                            }
+                            if (root.LeftChild != null)
+                            {
+                                roots.Push(root.LeftChild);
+                                Console.WriteLine($"Левая ветка со значением {root.LeftChild.Value} добавлена в стак");
+                            }
+                            if (roots.Count == 0)
+                            {
+                                Console.WriteLine("Очердь пуста, значение не найдено");
+                                Console.WriteLine("=====Конец DFS Поиска=====");
+                                return null;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Ветка со значением {root.Value} убирается из стака");
+                                root = roots.Pop();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Level_5/Tree.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level_5
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var node = obj as TreeNode;
+
+            if (node == null)
+                return false;
+
+            return node.Value == Value;
+        }
+    }
+
+    public interface ITree
+    {
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
+    }
+
+    public class Tree : ITree
+    {
+        TreeNode Root;
+        int RootCounter;
+        public void AddItem(int value)
+        {
+            TreeNode node = new TreeNode { Value = value };
+            if (Root == null)
+            {
+                Root = node;
+            }
+            else
+            {
+                if (GetNodeByValue(value) != null)
+                    Console.WriteLine("Значение уже записано");
+                else
+                {
+                    TreeNode root = Root;
+                    while (true)
+                    {
+                        if (value > root.Value)
+                        {
+                            if (root.RightChild != null)
+                                root = root.RightChild;
+                            else
+                            {
+                                root.RightChild = node;
+                                RootCounter++;
+                                break;
+                            }
+                        }
+                        if (value < root.Value)
+                        {
+                            if (root.LeftChild != null)
+                                root = root.LeftChild;
+                            else
+                            {
+                                root.LeftChild = node;
+                                RootCounter++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public TreeNode GetNodeByValue(int value)
+        {
+            TreeNode root = Root;
+            while (root != null)
+            {
+                if (root.Value == value)
+                {
+                    return root;
+                }
+                if (value > root.Value)
+                {
+                    if (root.RightChild != null)
+                        root = root.RightChild;
+                    else
+                        return null;
+                }
+                if (value < root.Value)
+                {
+                    if (root.LeftChild != null)
+                        root = root.LeftChild;
+                    else
+                        return null;
+                }
+            }
+            return null;
+        }
+
+        public TreeNode GetRoot()
+        {
+            return Root;
+        }
+        public void PrintTree()
+        {
+            string output = ($"-[{Root.Value}]");
+            Console.WriteLine(output);
+            int f = 0;
+            for (int i = 0; i < output.Length - 2; i++)
+            {
+                f++;
+            }
+            if (Root.RightChild != null)
+            {
+                for (int i = 0; i < output.Length - 2; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|-[R]-");
+                PrintNext(Root.RightChild, f + 4);
+            }
+            if (Root.LeftChild != null)
+            {
+                for (int i = 0; i < output.Length - 2; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|-[L]-");
+                PrintNext(Root.LeftChild, f + 4);
+            }
+        }
+        public void PrintNext(TreeNode root, int f)
+        {
+            string output = ($"[{root.Value}]");
+            Console.WriteLine(output);
+            int s = 0;
+            for (int i = 0; i < output.Length + f; i++)
+            {
+                s++;
+            }
+            if (root.RightChild != null)
+            {
+                for (int i = 0; i < output.Length + f; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|-[R]-");
+                PrintNext(root.RightChild, s + 4);
+            }
+            if (root.LeftChild != null)
+            {
+                for (int i = 0; i < output.Length + f; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|-[L]-");
+                PrintNext(root.LeftChild, s + 4);
+            }
+        }
+        public void RemoveItem(int value)
+        {
+            TreeNode root = Root;
+            TreeNode prevroot = null;
+            int l = 0;
+            if (GetNodeByValue(value) != null && GetNodeByValue(value) == Root)
+            {
+                Root = null;
+                RootCounter = 0;
+            }
+
+            while (root != null)
+            {
+                if (root.Value == value)
+                {
+                    if (l == 1)
+                    {
+                        prevroot.RightChild = null;
+                    }
+                    if (l == -1)
+                    {
+                        prevroot.LeftChild = null;
+                    }
+                    RootCounter--;
+                    break;
+                }
+                if (value > root.Value)
+                {
+                    if (root.RightChild != null)
+                    {
+                        l = 1;
+                        prevroot = root;
+                        root = root.RightChild;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Значение не найдено");
+                        break;
+                    }
+                }
+                if (value < root.Value)
+                {
+                    if (root.LeftChild != null)
+                    {
+                        l = -1;
+                        prevroot = root;
+                        root = root.LeftChild;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Значение не найдено");
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+    public class NodeInfo
+    {
+        public int Depth { get; set; }
+        public TreeNode Node { get; set; }
+    }
+    public static class TreeHelper
+    {
+        public static NodeInfo[] GetTreeInLine(ITree tree)
+        {
+            var bufer = new Queue<NodeInfo>();
+            var returnArray = new List<NodeInfo>();
+            var root = new NodeInfo() { Node = tree.GetRoot() };
+            bufer.Enqueue(root);
+
+            while (bufer.Count != 0)
+            {
+                var element = bufer.Dequeue();
+                returnArray.Add(element);
+
+                var depth = element.Depth + 1;
+
+                if (element.Node.LeftChild != null)
+                {
+                    var left = new NodeInfo()
+                    {
+                        Node = element.Node.LeftChild,
+                        Depth = depth,
+                    };
+                    bufer.Enqueue(left);
+                }
+                if (element.Node.RightChild != null)
+                {
+                    var right = new NodeInfo()
+                    {
+                        Node = element.Node.RightChild,
+                        Depth = depth,
+                    };
+                    bufer.Enqueue(right);
+                }
+            }
+            return returnArray.ToArray();
+        }
+    }
+}
